90ed95ee88bfa4abf746dd5ab0c5fa2cb334baf1
BloodKillerr <dawidslowik8@gmail.com>
2023-03-10 22:51:09 +0100

    10.03.2023 - Small fixes

--- Assets/Scripts/NotificationManager.cs
+++ Assets/Scripts/NotificationManager.cs
@@ -23,27 +23,41 @@ public class NotificationManager : MonoBehaviour
 
     public void SendNewDayNotification()
     {
-        AndroidNotification notification = new AndroidNotification();
-        notification.Title = "!";
-        notification.Text = "It's a new day! Don't forget to check your quests!";
+        if(!PlayerPrefs.HasKey("Message1"))
+        {
+            AndroidNotification notification = new AndroidNotification();
+            notification.Title = "!";
+            notification.Text = "It's a new day! Don't forget to check your quests!";
 
-        string newDate = string.Format("{0} 08:00:00", System.DateTime.Now.Date.AddDays(1).ToString("dd-MM-yyyy"));
-        notification.FireTime = System.DateTime.ParseExact(newDate, "dd-MM-yyyy HH:mm:ss", null);
-        notification.ShowTimestamp = true;
-        notification.RepeatInterval = new TimeSpan(24, 0, 0);
-        AndroidNotificationCenter.SendNotification(notification, "main_channel");
+            string newDate = string.Format("{0} 08:00:00", System.DateTime.Now.Date.AddDays(1).ToString("dd-MM-yyyy"));
+            notification.FireTime = System.DateTime.ParseExact(newDate, "dd-MM-yyyy HH:mm:ss", null);
+            notification.ShowTimestamp = true;
+            notification.RepeatInterval = new TimeSpan(24, 0, 0);
+            AndroidNotificationCenter.SendNotification(notification, "main_channel");
+            PlayerPrefs.SetInt("Message1", 1);
+            PlayerPrefs.Save();
+        }   
     }
 
     public void SendReminderNotification()
     {
-        AndroidNotification notification = new AndroidNotification();
-        notification.Title = "!";
-        notification.Text = "It's almost over! Don't forget about your daily routine!";
+        if(!PlayerPrefs.HasKey("Message2"))
+        {
+            AndroidNotification notification = new AndroidNotification();
+            notification.Title = "!";
+            notification.Text = "It's almost over! Don't forget about your daily routine!";
 
-        string newDate = string.Format("{0} 18:00:00", System.DateTime.Now.Date.AddDays(1).ToString("dd-MM-yyyy"));
-        notification.FireTime = System.DateTime.ParseExact(newDate, "dd-MM-yyyy HH:mm:ss", null);
-        notification.ShowTimestamp = true;
-        notification.RepeatInterval = new TimeSpan(24, 0, 0);
-        AndroidNotificationCenter.SendNotification(notification, "main_channel");
+            string newDate = string.Format("{0} 18:00:00", System.DateTime.Now.Date.AddDays(1).ToString("dd-MM-yyyy"));
+            notification.FireTime = System.DateTime.ParseExact(newDate, "dd-MM-yyyy HH:mm:ss", null);
+            notification.ShowTimestamp = true;
+            notification.RepeatInterval = new TimeSpan(24, 0, 0);
+            AndroidNotificationCenter.SendNotification(notification, "main_channel");
+            PlayerPrefs.SetInt("Message2", 1);
+            PlayerPrefs.Save();
+        }
+        else
+        {
+            return;
+        }
     }
 }
